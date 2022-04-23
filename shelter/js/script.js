8819aaeae9283bc150f9f5dc8b0ecc8403183cac
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-04-23 12:41:05 +0500

    feat: finish fetch json function

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -40,12 +40,13 @@ function activateBurgerMenu() {
 
 activateBurgerMenu();
 
+
 //=================== LINKS =============================
 
-function updateLinks() {
-  const linkBtns = document.querySelectorAll('.nav__link');
-  const helpLink = linkBtns[2];
-  const contacts = linkBtns[3];
+// function updateLinks() {
+//   const linkBtns = document.querySelectorAll('.nav__link');
+//   const helpLink = linkBtns[2];
+//   const contacts = linkBtns[3];
 
   //helpLink.removeAttribute('href');
   //contacts.removeAttribute('href');
@@ -55,6 +56,39 @@ function updateLinks() {
   // !!! just for main page !!!
   //const makeAFriendBtn = document.querySelector('.hero__content .btn__link');
   //makeAFriendBtn.setAttribute('href', './pets.html');
+//};
+
+//updateLinks()
+
+
+//=================== FETCH PETS Data =============================
+
+async function getData(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch(e) {
+    console.error(e)
+  }
 };
 
-updateLinks()
+//console.log(petsData);
+const petsData = await getData('../js/support/pets.json');
+
+
+// 1) CSS Carousel - Stream
+
+// 2) onload (make fn -> m.b. onresize later):
+//  - fetch json
+//  - random order cards arr (3/2/1)
+//  - compose cards
+//  - fill page
+
+// 3) arrow onclick:
+//  - add class slide
+//  - update current + next (random)
+//  - remove class
+
+// 4) card (wrapper) onclick -> ( compose + open ) modal + fix body
+//  - close modal
