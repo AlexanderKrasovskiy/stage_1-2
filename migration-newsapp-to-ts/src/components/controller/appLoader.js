ecf1c0ed0ca2e0434c42e8763389a5f64655d306
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-06-28 16:44:32 +0500

    feat: add newsAPI key to appLoader.js

--- migration-newsapp-to-ts/src/components/controller/appLoader.js
+++ migration-newsapp-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '4cdc102cbd074241b43b3659a534436e',
         });
     }
 }
