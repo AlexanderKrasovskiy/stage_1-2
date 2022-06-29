f87d28976b4f72dea534e390201fe62d406caae6
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-06-29 14:38:39 +0500

    feat: configure TS and ESLint for Webpack

---
+++ migration-newsapp-to-ts/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint', 'prettier', 'import'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        //'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
+    rules: {
+        'no-debugger': 'off',
+        'no-console': 'off',
+        'class-methods-use-this': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        'import/prefer-default-export': 'off',
+    },
+};
--- migration-newsapp-to-ts/webpack.config.js
+++ migration-newsapp-to-ts/webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+//const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, 'src', 'index'),
     mode: 'development',
     module: {
         rules: [
+            {
+                test: /\.[jt]s$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+                exclude: /node_modules/,
+            },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -15,19 +22,29 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
+            template: path.resolve(__dirname, 'src', 'index.html'),
             filename: 'index.html',
         }),
-        new CleanWebpackPlugin(),
+        new ESLintPlugin({
+            extensions: ['.ts', '.js'],
+            exclude: 'node_modules',
+        }),
+        // new CopyPlugin({
+        //     patterns: [{ from: './src/assets', to: '.dist/assets' }],
+        // }),
     ],
+    stats: {
+        errorDetails: true,
+    },
 };
 
 module.exports = ({ mode }) => {
