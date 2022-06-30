329ed7e266c0573bae47f245fd449d2b665d24b6
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-06-30 15:09:08 +0500

    feat: add separete interfaces for different API responses

--- migration-newsapp-to-ts/src/components/controller/loader.ts
+++ migration-newsapp-to-ts/src/components/controller/loader.ts
@@ -17,12 +17,35 @@ interface NewsSource {
     country: string;
 }
 
-interface ApiResponse {
-    status: 'ok' | 'error';
-    sources?: Readonly<NewsSource>[];
-    code?: number;
-    message?: string;
+interface Article {
+    source: { id: string | null; name: string };
+    author: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
 }
+
+interface SourcesResponse {
+    status: 'ok' | 'error';
+    sources: Readonly<NewsSource>[];
+}
+
+interface ArticlesResponse {
+    status: 'ok' | 'error';
+    totalResults: number;
+    articles: Readonly<Article>[];
+}
+
+interface ErrorResponse {
+    status: 'ok' | 'error';
+    code: string;
+    message: string;
+}
+
+type ApiResponse = SourcesResponse | ArticlesResponse | ErrorResponse;
 
 type CallBack = (data?: ApiResponse) => void;
 
