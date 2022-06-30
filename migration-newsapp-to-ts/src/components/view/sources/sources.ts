d8faea474f484e90a1f8481d76252559c54d8a3b
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-06-30 22:26:58 +0500

    feat: "add types to view - sources.ts"

---
+++ migration-newsapp-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import { NewsSource } from '../../../types/index';
+import './sources.css';
+
+class Sources {
+    draw(data: NewsSource[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLSpanElement).textContent = item.name;
+            sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')?.append(fragment);
+    }
+}
+
+export default Sources;
