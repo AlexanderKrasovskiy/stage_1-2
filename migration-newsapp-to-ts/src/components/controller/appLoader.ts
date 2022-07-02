12e9cb4ef7a4b4195e3f34c75ddd9d9c28974e5e
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-07-02 21:39:41 +0500

    refactor: change name in package.json to lowerCase

--- migration-newsapp-to-ts/src/components/controller/appLoader.ts
+++ migration-newsapp-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
+        // Original API link - https://newsapi.org/v2/
         super('https://nodenews.herokuapp.com/', {
-            // Original API link - https://newsapi.org/v2/
             apiKey: '4cdc102cbd074241b43b3659a534436e',
         });
     }
