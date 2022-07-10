d94921a6c8de235c61cb62afaaaf2f07511708f9
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-07-10 18:10:56 +0500

    feat: add product card and set image load

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,6 +1,6 @@
 import './scss/global.scss';
 
-//===========================================
+//=== SLIDER ========================================
 import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 
@@ -43,7 +43,44 @@ noUiSlider.create(yearSlider, {
 //   snapValues[idx].innerHTML = ('$' + Math.round(+handles[idx])) as string;
 // });
 
-console.log(priceSlider.noUiSlider?.get()); // GET str
-console.log(priceSlider.noUiSlider?.get(true)); // GET num
-priceSlider.noUiSlider?.set([300, 950]); // SET
+//console.log(priceSlider.noUiSlider?.get()); // GET str
+//console.log(priceSlider.noUiSlider?.get(true)); // GET num
+//priceSlider.noUiSlider?.set([300, 950]); // SET
 //priceSlider.noUiSlider?.reset(); // RESET
+
+// === IMAGES =====================================================
+
+const cache: Record<string, string> = {};
+// {./google/pixel-6-green.jpg: "http://127.0.0.1:5500/dist/assets/icons/pixel-6-green.jpg"}
+// {./google/pixel-6-green.jpg: "http://localhost:8080/assets/icons/pixel-6-green.jpg"}
+
+function importAll(r: __WebpackModuleApi.RequireContext) {
+  //console.log(r.keys()); // ['./google/pixel-6-green.jpg']
+
+  r.keys().forEach((key) => (cache[key] = r(key)));
+  console.log(cache);
+}
+
+importAll(require.context('./assets/images/', true, /\.(png|svg|jpg|jpeg|gif)$/i));
+
+// === TEST ADDING IMAGES ======================================================
+
+// const div = document.createElement('div');
+// div.style.width = '150px';
+// div.style.height = '150px';
+// div.style.margin = '20px';
+// div.style.padding = '20px';
+// div.style.backgroundSize = 'contain';
+// div.style.backgroundRepeat = 'no-repeat';
+
+// div.style.backgroundImage = `url('./assets/icons/pixel-6-green.jpg')`;
+// document.body.prepend(div);
+
+// === CLONE CARDS ==================================
+// let products = document.querySelector('.products');
+// let card = document.querySelector('.card');
+
+// for (let i = 0; i < 20; i++) {
+//   let copy = card.cloneNode(true);
+//   products.append(copy);
+// }
--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/img/[name][ext][query]',
+          filename: 'assets/icons/[name][ext][query]',
         },
       },
       {
