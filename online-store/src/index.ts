9481cf768afdac38466594b5d7557b392ac3ad49
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-07-14 14:37:15 +0500

    feat: add self assessment to console

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,5 +1,8 @@
 import Controller from './components/controller';
 import './scss/global.scss';
+import { logScore } from './components/selfAssessment';
 
 const app = new Controller();
 app.start();
+
+logScore();
